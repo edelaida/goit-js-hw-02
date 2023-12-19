7d959bdb13bd307cb95d1a732e9e3134b0c1c60d
Levina Tani <edelaida@gmail.com>
2023-12-19 18:34:10 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+// Оголоси функцію getShippingMessage, яка очікує три параметри,
+//     значення яких будуть задаватися під час її виклику:
+
+// country — перший параметр, рядок, що містить країну доставки
+// price — другий параметр, число, що містить загальну вартість товару
+// deliveryFee — третій параметр, число, що містить вартість доставки товару
+// Доповни код функції так, щоб вона повертала рядок з повідомленням
+// про доставку товару в країну користувача:
+// "Shipping to <country> will cost <totalPrice> credits", де:
+
+// <country> — це країни доставки
+// <totalPrice> — це загальна вартість замовлення, що включає вартість
+// товару і його доставки
+// Візьми код нижче і встав після оголошення своєї функції
+// для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+// console.log(getShippingMessage("Australia", 120, 50));
+// console.log(getShippingMessage("Germany", 80, 20));
+// console.log(getShippingMessage("Sweden", 100, 20));
+
+function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = (price + deliveryFee);
+    return(`Shipping to ${country} will cost ${totalPrice} credits`)
+}
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
+
