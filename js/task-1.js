465a9632f233b72898486cd94f58d66b11d975ce
Levina Tani <edelaida@gmail.com>
2023-12-25 13:03:59 +0200

    message

--- js/task-1.js
+++ js/task-1.js
@@ -15,12 +15,6 @@
 // тобто вартість усіх замовлених дроїдів
 // Візьми код нижче і встав після оголошення своєї функції
 // для перевірки коректності її роботи.У консоль будуть виведені результати її роботи.
-
-// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
-
 // function makeTransaction(quantity, pricePerDroid) {
 //     const totalPrice = (quantity * pricePerDroid);
 //     return (`You ordered ${quantity} droids worth ${totalPrice} credits!`);
@@ -29,9 +23,7 @@
 // console.log(makeTransaction(3, 1000));
 // console.log(makeTransaction(10, 500));
 
-
-
-//*************** */
+//*****************************************
 // Задача 1. Замовлення дроїдів
 
 // Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати
@@ -57,19 +49,37 @@
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності
 // її роботи.У консоль будуть виведені результати її роботи.
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let totalSum = quantity * pricePerDroid;
-    if (totalSum <= customerCredits) {
-       return (`You ordered ${quantity} droids worth ${totalSum} credits!`);
-    } else {
-        return (`Insufficient funds!`);  
-    }
- }
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//     let totalSum = quantity * pricePerDroid;
+//     if (totalSum <= customerCredits) {
+//        return (`You ordered ${quantity} droids worth ${totalSum} credits!`);
+//     } else {
+//         return (`Insufficient funds!`);
+//     }
+//  }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
 
+// ***************************** 1 *************
+
+// Slug — це завжди рядок у нижньому регістрі, слова якого розділені тире.
+
+// Напиши функцію slugify(title), яка приймає заголовок статті,
+// параметр title і повертає slug, створений із цього рядка.
+
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки 
+// коректності її роботи.У консоль будуть виведені результати її роботи.
+
+    
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
 
